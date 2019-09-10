53f6c5124e98fbae9c28cc804daf63ab4461a640
sujata <sujata.y@brillio.com>
2019-09-10 21:34:34 +0530

    added review

--- src/components/BusinessSearch.js
+++ src/components/BusinessSearch.js
@@ -1,40 +1,59 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 
- class BusinessSearch extends Component{
-     constructor(props){
-         super(props)
-         this.state = {
-             posts : [
-                
-             
-             ]
-         
+class BusinessSearch extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            posts: [
+            ],
+            reviews:[]
         }
-    
-     }
-     componentDidMount(){
-         //const token = '3RiPRT-xv5Dbp67NuIzp43JNWsep0Ee7v2HXFstyKW1cnGyn9dQOaT2QJvFj-6WNaK5dJvm67C5aG080nqIsqY61DgIp_EP6YjM17PoADxa4GVt3cABgskTwOY52XXYx';
-         //const api = "https://api.yelp.com/v3/businesses/search?term=icecream&location=Alpharetta";
-        // axios.defaults.headers["content-type"] = "application/json";
-      //  axios.defaults.headers.common.authorization = `Bearer ${token}`;         axios.get(api)
-    //  axios.get("http://localhost:3000/api/posts")  
-    axios.get("https://api.yelp.com/v3/businesses/search?term=icecream&location=Alpharetta")
-      .then(res =>{
-             //this.setState({posts : Response.business})
-             //console.log("res is ::",res);
-         })
-         .catch()
-     }
-    render(){
-        const { posts} = this.state;
-        return(
+        
+
+
+
+    }
+    componentDidMount() {
+        const token = '3RiPRT-xv5Dbp67NuIzp43JNWsep0Ee7v2HXFstyKW1cnGyn9dQOaT2QJvFj-6WNaK5dJvm67C5aG080nqIsqY61DgIp_EP6YjM17PoADxa4GVt3cABgskTwOY52XXYx';
+        //const api = "https://api.yelp.com/v3/businesses/search?term=icecream&location=Alpharetta";
+        axios.defaults.headers["content-type"] = "application/json";
+        axios.defaults.headers.common.authorization = `Bearer ${token}`;
+        //  axios.get("http://localhost:3000/api/posts")  
+        axios.get("https://api.yelp.com/v3/businesses/search?term=icecream&location=Alpharetta&limit=5")
+            .then(res => {
+                let result = res.slice(0, 5); // if not in query parameter
+                this.setState({ posts: result.businesses })
+                
+            })
+            .catch()
+
+         axios.get("https://api.yelp.com/v3/businesses/AOG6RgzwBFqn8doIeID6zQ/reviews")
+         .then(data=>{
+             this.setState({reviews : data.reviews})
+         }) 
+         .catch()  
+    }
+    render() {
+        const { posts } = this.state.posts;
+        const { reviews } = this.state.reviews;
+        return (
             <div>
                 List of BusinessSearch
-               
-                posts.map(post => <div key={posts.id}>{posts.name}</div>):null
+                posts.map(post => <span key={posts.id}>{posts.name}
+                    {posts.name}
+                    {posts.rating}
+                    {posts.location.city}
+                    {posts.display_address}
+                    {posts.review_count}
+                    {posts.coordinates.latitude}
+                    {posts.coordinates.longitude}
+                    {posts.phone}
+                    {posts.display_phone}
+                    {posts.distance})</span>
+                 Review By Person: {reviews.user.name}
             </div>
-        )
+        );
     }
 }
 export default BusinessSearch
